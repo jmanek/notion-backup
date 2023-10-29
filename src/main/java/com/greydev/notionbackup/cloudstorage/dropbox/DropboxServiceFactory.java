c161a732cc239907954b0fc7a5a9b6bf40bdde91
Jesse Manek <jmanek@users.noreply.github.com>
2023-10-29 12:06:32 -0700

    Update DropboxServiceFactory.java

--- src/main/java/com/greydev/notionbackup/cloudstorage/dropbox/DropboxServiceFactory.java
+++ src/main/java/com/greydev/notionbackup/cloudstorage/dropbox/DropboxServiceFactory.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.apache.commons.lang3.StringUtils;
 
 import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.oauth.DbxCredential
 import com.dropbox.core.v2.DbxClientV2;
 
 import lombok.extern.slf4j.Slf4j;
@@ -13,15 +14,13 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class DropboxServiceFactory {
 
-	public static Optional<DbxClientV2> create(String dropboxAccessToken) {
-		if (StringUtils.isBlank(dropboxAccessToken)) {
-			log.warn("The given dropboxAccessToken is blank.");
-			return Optional.empty();
-		}
+	public static Optional<DbxClientV2> create(String dropboxAccessToken, String dropboxRefreshToken, String dropboxAppKey, String dropboxAppSecret) {
 		DbxClientV2 service = null;
+		DbxCredential credential = null
 		try {
+			credential = new DbxCredential(dropboxAccessToken, -1L, dropboxRefreshToken, dropboxAppKey, dropboxAppSecret);
 			DbxRequestConfig config = DbxRequestConfig.newBuilder("dropbox/notion-backup").build();
-			service = new DbxClientV2(config, dropboxAccessToken);
+			service = new DbxClientV2(config, credential);
 		} catch (Exception e) {
 			log.warn("An exception occurred: ", e);
 		}
